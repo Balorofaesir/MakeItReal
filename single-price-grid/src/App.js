1dbdb75b219750b27cb8c2d003d2d8fc89802050
Baldorofaesir <salomoneyd@gmail.com>
2022-10-25 11:22:04 -0500

    adding React Exercise Single Price Grid

---
+++ single-price-grid/src/App.js
@@ -0,0 +1,13 @@
+import SinglePriceGrid from './components/SinglePriceGrid';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <SinglePriceGrid/>
+    </div>
+  );
+}
+
+export default App;
