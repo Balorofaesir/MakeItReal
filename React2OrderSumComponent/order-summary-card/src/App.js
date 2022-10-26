aa4d676c36359c65b25dc6baf7329df82ef216e6
Baldorofaesir <salomoneyd@gmail.com>
2022-10-26 14:02:11 -0500

    Adding react 2 Ex

---
+++ React2OrderSumComponent/order-summary-card/src/App.js
@@ -0,0 +1,10 @@
+import OrderSummaryCard from './components/orderSummaryCard/index.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <OrderSummaryCard />
+  );
+}
+
+export default App;
